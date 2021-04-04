d78e6625727f41b7971d475b11b81077a4b29341
serpent-charmer <zagirov.robert@yandex.ru>
2021-04-04 08:46:45 -0400

    Edited sql editor error display

---
+++ scripts/refresh_db_containers.py
@@ -0,0 +1,17 @@
+import docker
+
+def refresh_container(client, containers_names):
+
+    failed = []
+    for cname in containers_names:
+        try:
+            ct = client.containers.get(cname)
+            ct.restart()
+        except docker.errors.NotFound:
+            failed = [*failed, cname]
+    return failed
+
+if __name__ == '__main__':
+    client = docker.from_env()
+    failed = refresh_container(client, ['some-mysql', 'some-postgres'])
+    print(failed)
